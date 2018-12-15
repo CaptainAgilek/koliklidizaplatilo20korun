819928b5adc79fa513698b5e49b1c01f0fe51566
CaptainAgilek <martinsedlacek7@gmail.com>
2018-12-15 16:04:43 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Kolik lidi zaplatilo 20 korun, aby videli, kolik lidi zaplatilo 20 korun?</title>
+    <meta name="description" content="20korun">
+
+
+</head>
+
+<body>
+<?php
+echo '<p>Hello World</p>';
+?>
+</body>
+</html>
