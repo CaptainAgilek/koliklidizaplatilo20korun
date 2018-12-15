41899839666ff09debf59db3a34b4b156371e9f1
CaptainAgilek <martinsedlacek7@gmail.com>
2018-12-15 17:16:01 +0100

    update index

--- index.php
+++ index.php
@@ -23,12 +23,13 @@
             <script
                     src="https://checkout.stripe.com/checkout.js" class="stripe-button"
                     data-key="pk_test_TYooMQauvdEDq54NiTphI7jx"
-                    data-amount="999"
                     data-name="Stripe.com"
                     data-description="Widget"
                     data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
                     data-locale="auto"
-                    data-zip-code="true">
+                    data-zip-code="true"
+                    data-amount="20"
+                    data-currency="CZK">
             </script>
         </form>
     </div>
