a760724ee4016738d5389b31f37fea3daaf502b5
CaptainAgilek <martinsedlacek7@gmail.com>
2018-12-15 16:23:13 +0100

    update index

--- index.php
+++ index.php
@@ -7,13 +7,13 @@
     <title>Kolik lidi zaplatilo 20 korun, aby videli, kolik lidi zaplatilo 20 korun?</title>
     <meta name="description" content="20korun">
 
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 
 </head>
 
 <body>
     <div class="content-wrapper" >
-        <h1>Kolik lidí zaplatilo 20 korun, aby viděli, kolik lidí zaplatilo 20 korun?</h1>
+        <h1>Kolik lidí zaplatilo 20 korun,<br> aby viděli, kolik lidí zaplatilo 20 korun?</h1>
     </div>
 <?php
 echo '<p>Hello World</p>';
